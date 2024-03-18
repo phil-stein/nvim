d5c5fed8f8d31b44cf8a4010237704694c002f49
Phil Desktop <philipp033.25@gmail.com>
2024-03-18 19:33:26 +0100

    DESKTOP: syncing config

--- init.lua
+++ init.lua
@@ -1459,9 +1459,25 @@ mason_lspconfig.setup_handlers {
       on_attach = on_attach,
       settings = servers[server_name],
       filetypes = (servers[server_name] or {}).filetypes,
+      icons_enabled = true,
     }
   end,
 }
+
+-- lsp diagnostic display settings
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+  -- update_in_insert = false,
+  -- severity_sort = false,
+})
+-- lsp error, warning, info, etc, icons
+local signs = { Error = "󰅚 ", Warn = "󰀪 ", Hint = "󰌶 ", Info = " " }
+for type, icon in pairs(signs) do
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+end
 
 -- [[ Configure nvim-cmp ]]
 -- See `:help cmp`
